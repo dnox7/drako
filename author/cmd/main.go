3f3a5f1b44faed7b8b925647b257e0912ed8b87d
Dang Hai <pecheule34713@gmail.com>
2025-07-06 17:42:47 +0700

    dev: demo grpc

---
+++ author/cmd/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+
+	pb "github.com/dnox7/drako/contracts/gen/go/stub/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+var authorsData = []*pb.Author{
+	{
+		Id:   1,
+		Name: "Tom",
+		Age:  12,
+	},
+	{
+		Id:   2,
+		Name: "Jerry",
+		Age:  12,
+	},
+}
+
+type authorServer struct {
+	pb.UnimplementedAuthorServiceServer
+	data []*pb.Author
+}
+
+func (s *authorServer) GetAuthor(ctx context.Context, req *pb.GetAuthorRequest) (*pb.GetAuthorResponse, error) {
+	for _, author := range s.data {
+		if author.Id == req.Id {
+			return &pb.GetAuthorResponse{
+				Author: author,
+			}, nil
+		}
+	}
+	return nil, errors.New("record not found")
+}
+
+func (s *authorServer) ListAuthors(ctx context.Context, _ *emptypb.Empty) (*pb.ListAuthorsResponse, error) {
+	return &pb.ListAuthorsResponse{
+		Authors: s.data,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterAuthorServiceServer(grpcServer, &authorServer{
+		data: authorsData,
+	})
+	grpcServer.Serve(lis)
+}
---
+++ book/cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	pb "github.com/dnox7/drako/contracts/gen/go/stub/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type authorService struct {
+	client pb.AuthorServiceClient
+	logger *log.Logger
+}
+
+func (s *authorService) GetAuthorByID(ctx context.Context, id int32) {
+	author, err := s.client.GetAuthor(ctx, &pb.GetAuthorRequest{
+		Id: id,
+	})
+	if err != nil {
+		s.logger.Printf("failed to get author by id: %v", err)
+	}
+	fmt.Printf("%+v\n", author)
+}
+
+func (s *authorService) GetAllAuthors(ctx context.Context) {
+	authors, _ := s.client.ListAuthors(ctx, nil)
+	fmt.Println("%+v\n", authors)
+}
+
+func main() {
+	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewAuthorServiceClient(conn)
+
+	authorService := &authorService{
+		client: client,
+		logger: log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile),
+	}
+
+	authorService.GetAuthorByID(context.Background(), 1)
+	authorService.GetAllAuthors(context.Background())
+}
